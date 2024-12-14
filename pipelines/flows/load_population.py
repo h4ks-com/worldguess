c6adb3b9c08214224b2aa8d4b092fc9f5b4e6328
Matheus Fillipe <matheusfillipeag@gmail.com>
2024-12-14 12:03:08 +0100

    reformat

--- pipelines/flows/load_population.py
+++ pipelines/flows/load_population.py
@@ -2,7 +2,10 @@ from backend.worldguess.constants import PIPELINE_READYNESS_KEY
 
 from .base import Job, JobStatus, RunStatusType
 
-WORLDPOP_POPULATION_DENSITY="https://data.worldpop.org/GIS/Population/Global_2000_2020/2020/0_Mosaicked/ppp_2020_1km_Aggregated.tif"
+WORLDPOP_POPULATION_DENSITY = (
+    "https://data.worldpop.org/GIS/Population/Global_2000_2020/2020/0_Mosaicked/ppp_2020_1km_Aggregated.tif"
+)
+
 
 class LoadPopulation(Job):
     """Sets done status in the cache."""
